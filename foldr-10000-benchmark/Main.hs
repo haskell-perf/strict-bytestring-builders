c1d20989378e49fab0bb59ab1d89bac66a5c83e0
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-03-19 14:46:37 +0300

    Add more benchmarks

---
+++ foldr-10000-benchmark/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Prelude
+import Criterion.Main
+import qualified ByteString.BuildersBenchmark.Benchmarks as A
+import qualified ByteString.BuildersBenchmark.Actions as B
+
+
+main =
+  defaultMain $
+  [
+    A.action "foldr" (B.foldr 10000)
+  ]
